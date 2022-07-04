27b7805f9bb330958c7d4f4de32ad770e52318e5
koichi.sakamoto <sakamoto0321@gmail.com>
2022-07-04 10:50:02 +0900

    受入待ち処理ロジックを変更

--- docker/check_jobs.sh
+++ docker/check_jobs.sh
@@ -2,19 +2,21 @@
 # Check jobs acceptance timeout.
 # If jobs cannot be confirmed, listening will be terminated.
 
-if [ -z "$1" ]; then exit 1; fi
-
-_RUNNER_WORKDIR=${RUNNER_WORKDIR:-/_work}
 _JOBS_ACCEPTANCE_TIMEOUT=$1
+_CHECK_DIR=/actions-runner/_diag/pages
 
-while [ 1 ]; do
-  if [ ! -e ${_RUNNER_WORKDIR}/_temp/_github_workflow ]; then
-    _DURATION=`expr $(date +%s) - $(date +%s -r /tmp)`
-    [[ ${_DURATION} -ge ${_JOBS_ACCEPTANCE_TIMEOUT} ]] && break
-  fi
+sleep 1
+while [[ -n $(pgrep Runner.Listener) ]]; do
   sleep 10
-done
+  _NOW="$(date +%s)"
+  _LASTUPDATE="$(date +%s -r /tmp)"
+  _DURATION=$((${_NOW} - ${_LASTUPDATE}))
+  [[ -e ${_CHECK_DIR} ]] && [[ -n "$(ls -A ${_CHECK_DIR})" ]] && continue
+  [[ ${_DURATION} -lt ${_JOBS_ACCEPTANCE_TIMEOUT} ]] && continue
 
-echo $(date '+%Y-%m-%d %H:%M:%SZ:') Stop listening due to timeout.
-pkill --signal=SIGINT -f ./bin/Runner.Listener
+  echo "$(date '+%Y-%m-%d %H:%M:%SZ:') Stop listening due to timeout."
+  pkill --signal=SIGINT -f ./bin/Runner.Listener
+  exit 0
+done
+echo "$(date '+%Y-%m-%d %H:%M:%SZ:') Skip check jobs."
 exit 0
