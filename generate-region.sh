8afdc7f47db276bee0a8cdc8cb000f7f84a0151d
koichi.sakamoto <sakamoto0321@gmail.com>
2022-07-04 12:05:23 +0900

    fix code check

--- generate-region.sh
+++ generate-region.sh
@@ -11,8 +11,8 @@ terraform=${DIR}/terraform
 
 region=$1
 if [ -n "$region" ]; then
-  if [ -e ${terraform}/${region} ]; then
-    echo already exists directory of region \"${region}\".
+  if [ -e "${terraform}/${region}" ]; then
+    echo "already exists directory of region \"${region}\"."
     exit 1
   fi
 else
@@ -20,8 +20,8 @@ else
   select region in us-east-1 us-east-2 us-west-1 us-west-2 af-south-1 ap-east-1 ap-south-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-southeast-1 ap-southeast-2 ca-central-1 cn-north-1 cn-northwest-1 eu-central-1 eu-west-1 eu-west-2 eu-west-3 eu-south-1 eu-north-1 me-south-1 sa-east-1
   do
     if [ -n "$region" ]; then
-      if [ -e ${terraform}/${region} ]; then
-        echo already exists directory of region \"${region}\".
+      if [ -e "${terraform}/${region}" ]; then
+        echo "already exists directory of region \"${region}\"."
       else
         break
       fi
@@ -29,13 +29,15 @@ else
   done
 fi
 
-mkdir -p ${DIR}/tmp
-rm -rf ${DIR}/tmp/*
-cp -r ${DIR}/template/* ${DIR}/tmp
+mkdir -p "${DIR}/tmp"
+rm -rf "${DIR}/tmp/"*
+cp -r "${DIR}/template/"* "${DIR}/tmp"
 
-for tgt in `find ${DIR}/tmp/* -type f`; do
-  sed -i -e "s/<region>/${region}/" $tgt
-done
-rename "s/\[\[region\]\]/${region}/" ${DIR}/tmp/*
-mv ${DIR}/tmp/* ${terraform}/
-rmdir ${DIR}/tmp/
+while IFS= read -r -d '' tgt
+do
+  sed -i -e "s/<region>/${region}/" "${tgt}"
+done < <(find "${DIR}/tmp/"* -type f -print0)
+
+rename "s/\[\[region\]\]/${region}/" "${DIR}/tmp/"*
+mv "${DIR}/tmp/"* "${terraform}/"
+rmdir "${DIR}/tmp/"
