98e1cde59aa11f4e590fe636dbbede839443cbe8
sakamoto66 <sakamoto0321@gmail.com>
2022-11-25 11:57:50 +0900

    定義誤りを修正

--- template/[[region]]/batch.tf
+++ template/[[region]]/batch.tf
@@ -3,8 +3,8 @@ module "batch" {
   source = "../batch"
 
   batch_name                   = var.batch_name
-  self_hosted_runner_url        = var.self_hosted_runner_url
-  self_hosted_runner_secret_arn = var.self_hosted_runner_secret_arn
+  github_self_runner_url        = var.self_hosted_runner_url
+  github_self_runner_secret_arn = var.self_hosted_runner_secret_arn
 
   batch_service_role_arn        = var.batch_service_role_arn
   batch_service_role_attachment = var.batch_service_role_attachment
