861b87c59109547b6aab0c434bc7d54ee60050d5
koichi.sakamoto <sakamoto0321@gmail.com>
2022-11-26 01:03:26 +0900

    tfstateのS3対応

--- terraform/base.tf
+++ terraform/base.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 1.2.2"
+  backend "s3" {
+    bucket = "aws-batch-github-runner"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
