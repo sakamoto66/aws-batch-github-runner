cea25cc26871f26dc7474c3339d58a180514e551
sakamoto66 <sakamoto0321@gmail.com>
2022-11-25 12:19:17 +0900

    定義誤りを修正修正

--- template/[[region]].tf
+++ template/[[region]].tf
@@ -2,8 +2,8 @@ module "<region>" {
   source = "./<region>"
 
   batch_name                    = var.batch_name
-  github_self_runner_url        = var.self_hosted_runner_url
-  github_self_runner_secret_arn = var.self_hosted_runner_secret_arn
+  self_hosted_runner_url        = module.runner_ecr.repository_url
+  self_hosted_runner_secret_arn = var.self_hosted_runner_secret_arn
 
   batch_service_role_arn        = module.iam_role.batch_service_role_arn
   batch_service_role_attachment = module.iam_role.batch_service_role_attachment
