2a3527c5f70b12052bf11bc712ebb5956795f21f
koichi.sakamoto <sakamoto0321@gmail.com>
2022-11-25 22:43:52 +0900

    参照誤りを修正

--- template/[[region]].tf
+++ template/[[region]].tf
@@ -3,7 +3,7 @@ module "<region>" {
 
   batch_name                    = var.batch_name
   self_hosted_runner_url        = module.runner_ecr.repository_url
-  self_hosted_runner_secret_arn = var.self_hosted_runner_secret_arn
+  self_hosted_runner_secret_arn = var.secret_arn
 
   batch_service_role_arn        = module.iam_role.batch_service_role_arn
   batch_service_role_attachment = module.iam_role.batch_service_role_attachment
