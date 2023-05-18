d8c8719939f885a2de6e370942a1e65925b708c4
koichi.sakamoto <sakamoto0321@gmail.com>
2023-05-18 09:00:58 +0900

    change aws_default_security_group

--- template/[[region]]/vpc.tf
+++ template/[[region]]/vpc.tf
@@ -7,33 +7,26 @@ module "vpc" {
   azs            = ["<region>a"]
   public_subnets = ["10.0.101.0/24"]
   map_public_ip_on_launch = true
-  
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
 
-resource "aws_default_security_group" "default" {
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [module.vpc.vpc_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  default_security_group_ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = "${module.vpc.vpc_cidr_block}"
+    }
+  ]
+  default_security_group_egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = "0.0.0.0/0"
+    }
+  ]
 
   tags = {
-    Name = "runner-vpc-default"
     Terraform   = "true"
     Environment = "dev"
   }
